65820781f96a1aae7095c517f58ca608399f4a4b
Emer17 <emer-martins@hotmail.com>
2016-09-20 09:30:55 -0300

    adicionado arquivo de teste de criacao de processos

---
+++ src/teste.c
@@ -0,0 +1,21 @@
+#include "types.h"      //talvez seja necessario para usar alguns tipos de variaveis
+#include "user.h"       //para usar funcoes como printf e syscalls
+
+#define STDOUT 1       
+
+int main(){
+    int pid = fork();
+    if(pid > 0){
+        printf(STDOUT,"parent: child=%d\n", pid);
+        pid = wait();
+        printf(STDOUT,"child %d is done\n", pid);
+    } else if(pid == 0){
+        printf(STDOUT,"child: exiting\n");
+        exit();
+    } else {
+        printf(STDOUT,"fork error\n");
+    }
+    
+    
+    exit();
+}
